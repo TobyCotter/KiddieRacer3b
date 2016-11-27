c332291fd09e4c9744b943eee5eb1e166dcdbf84
6420 Laptop\Toby C <prog4life75@gmail.com>
2016-11-26 20:44:59 -0500

    Created new method that handles moving in X axis.  Z axis still uses translate.  X uses a lerping method on it's transform position.

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	// Variables
 	private float totalTimeSinceCollision;
+	private float desiredXPos;
 	private float zSpeed;
 	private float[] lane_XValue = {96.65f, 98.9f, 101.15f, 103.4f};
 	private bool leftArrow = false;
 	private bool rightArrow = false;
-	private int LaneLerpLoop = 0;
 	private int myLanePos;
 	private float engineSoundPitch;
 	private RaceManager raceManager;
@@ -16,8 +16,8 @@ public class Player : MonoBehaviour {
 	private Shooter shooter;
 	private Animator anim;
 	private bool rayCastLeftResult;
-	private bool OkToMoveLeft, OkToMoveRight = false;
-	//private bool rayCastRightResult;
+	private bool OkToMoveLeft;
+	private bool OkToMoveRight;
 
 	[Tooltip("Speed player moves in Z")]
 	[Range(0f,100f)]
@@ -26,7 +26,7 @@ public class Player : MonoBehaviour {
 	[Range(0f,30.0f)]
 	public float LaneLerpSpeed;
 	public float zGear = 1.1f;							// Which gear the player car is in, 1.1 = 1st, 1.2 = 2nd
-	public int origLaneNumber;
+	//public int origLaneNumber;
 	private RaycastHit rearHit;
 	private RaycastHit frontHit;
 
@@ -37,18 +37,17 @@ public class Player : MonoBehaviour {
 		shooter = GameObject.FindObjectOfType<Shooter>();
 		engineAudioSource = GetComponent<AudioSource>();
 		anim = GetComponent<Animator>();
+		desiredXPos = transform.position.x;				//desiredXPos isn't assigned a value until we push left or right, need default value
 	}
 	
 
 	void Update () {
-		//Debug.DrawRay(transform.position, Vector3.right * 2.5f);
-
 		if(raceManager.raceHasBegun){					//Only true when race has begun
 			totalTimeSinceCollision = totalTimeSinceCollision + Time.deltaTime;
 			ChooseGear();
 			DetectArrowKey();			
-			MoveLeftOrRight();
-			MovePlayerForward();
+			MovePlayerZAxis();
+			MovePlayerXAxis();
 			anim.SetBool("isRacingBool", true);			// Starts moving tires and vertical bounce is less
 		}
 	}// End Update
@@ -96,15 +95,45 @@ public class Player : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.LeftArrow)){
 			leftArrow = true;
 			rightArrow = false;
-			LaneLerpLoop = 0;
 		}
 
 		if(Input.GetKeyDown(KeyCode.RightArrow)){
 			rightArrow = true;
 			leftArrow = false;
-			LaneLerpLoop = 0;
 		}
-	}// End ChangeLanes ()
+	}//End
+
+
+	private void MovePlayerZAxis (){
+		float speedBurstMultiplier = 1.0f;
+		float newSpeedMultiplier;
+		float oldSpeedMultiplier;
+
+		//Purpose of the following is to lerp the rocket speed burst
+		newSpeedMultiplier = shooter.speedBurst;
+		oldSpeedMultiplier = speedBurstMultiplier;
+		speedBurstMultiplier = Mathf.Lerp(oldSpeedMultiplier, newSpeedMultiplier, .8f);
+
+		//Calculate total Z speed
+		zSpeed = (playerSpeedOffset * Time.deltaTime) * zGear * speedBurstMultiplier;
+
+		//Move Player in Z axis
+		transform.Translate(0,0,zSpeed);
+	}// End
+
+
+	private void MovePlayerXAxis(){
+		if(leftArrow && myLanePos >= 2){							// To move left we need to be in atleast the 2nd lane
+			myLanePos--;
+			leftArrow = false;
+		}else if(rightArrow && myLanePos <= 3){						// To move right we need to be in the 3rd lane or less
+			myLanePos++;
+			rightArrow = false;
+		}
+
+		desiredXPos = Mathf.Lerp(transform.position.x, lane_XValue[myLanePos-1], (LaneLerpSpeed * Time.deltaTime));			
+		transform.position = new Vector3(desiredXPos, transform.position.y, transform.position.z);						//Move our car in the x axis only
+	}//End
 
 
 	private void RayCastLeft(){
@@ -143,91 +172,5 @@ public class Player : MonoBehaviour {
 		Debug.Log("Front Right raycast: " + frontRayHitResult);
 		Debug.Log("Rear right raycast: " + rearRayHitResult);
 	}//End Raycast
+}//End Class
 
-
-//	if(hit.collider.GetComponent<Dummy>()){
-//				Debug.Log("We do not have a dummy!!");
-//			}
-
-
-	private void MoveLeftOrRight (){
-		float desiredXPos;
-		int lerpPlayerThisManyFrames = 4;
-		//TODO we have the leftArrow and rightArrow being latched on
-		if(leftArrow){														//True if left arrow key has been pressed
-			if(LaneLerpLoop == 0){											//We do this because otherwise we rayCast 5 times, we only need one raycast
-				RayCastLeft();												//Checks for colliders one lane to the left of us
-			}
-			if(OkToMoveLeft){
-				//Move one lane left
-				if(LaneLerpLoop < lerpPlayerThisManyFrames){				// Will move the player this many frames
-					// Change lanes to the left while lerping
-					if(myLanePos >= 2){										// This prevents us from moving our car out of the road
-						desiredXPos = Mathf.Lerp(transform.position.x, lane_XValue[myLanePos-2], (LaneLerpSpeed * Time.deltaTime));		// Minus 2 because one if because of the array and other is because this is our target lane
-						transform.position = new Vector3(desiredXPos, transform.position.y, transform.position.z);
-						LaneLerpLoop++;
-					}
-				}else{
-					transform.position = new Vector3(lane_XValue[myLanePos-2], transform.position.y, transform.position.z);		//Makes lane change x value complete
-					leftArrow = false;										// Stop changing lanes to the left
-					origLaneNumber = myLanePos;								// Used to calculate collisions
-					myLanePos -= 1;
-				}
-			}
-		}// End leftArrow
-
-		if(rightArrow){														//True if right arrow key has been pressed
-			if(LaneLerpLoop == 0){											//We do this because otherwise we rayCast 5 times, we only need one raycast
-				RayCastRight();												//Checks for colliders to the right of us
-			}
-			if(OkToMoveRight){												//If there's nothing to the right of us, then we will move to the right
-				if(LaneLerpLoop < lerpPlayerThisManyFrames){
-					// Change lanes to the right while lerping
-					if(myLanePos <= 3){																				// This prevents us from moving our car out of the road
-						desiredXPos = Mathf.Lerp(transform.position.x, lane_XValue[myLanePos], LaneLerpSpeed);		// We do not do a [myLanePos - 2] because we are turning right
-						transform.position = new Vector3(desiredXPos, transform.position.y, transform.position.z);
-						LaneLerpLoop++;
-					}
-				}else{
-					transform.position = new Vector3(lane_XValue[myLanePos], transform.position.y, transform.position.z);	//Moves car to exact x pos (lerping only moved it part of the way)
-					rightArrow = false;											// Stop changing lanes to the right
-					origLaneNumber = myLanePos;									// Used to calculate collisions
-					myLanePos += 1;
-				}
-			}
-		}// End rightArrow
-	}// MoveLeftOrRight ()
-
-
-	private void MovePlayerForward (){
-		//TODO Instead of lerping left and right in a separate function, let's move left or right here
-
-
-		float speedBurstMultiplier = 1.0f;
-		float newSpeedMultiplier;
-		float oldSpeedMultiplier;
-
-		//Purpose of the following is to lerp the speed burst
-		newSpeedMultiplier = shooter.speedBurst;
-		oldSpeedMultiplier = speedBurstMultiplier;
-		speedBurstMultiplier = Mathf.Lerp(oldSpeedMultiplier, newSpeedMultiplier, .8f);
-
-		zSpeed = (playerSpeedOffset * Time.deltaTime) * zGear * speedBurstMultiplier;
-		transform.Translate(0,0,zSpeed);
-	}// End MovePlayerForward
-}
-
-
-
-
-//public void MovePlayerBackToOriginalLane(){
-//		float desiredXPos;
-//		int lerpPlayerThisManyFrames = 4;
-//
-//		for (int i = 0; i < lerpPlayerThisManyFrames; i++) {
-//			desiredXPos = Mathf.Lerp(transform.position.x, lane_XValue[origLaneNumber-1], (LaneLerpSpeed * Time.deltaTime));	
-//		}
-//		transform.position = new Vector3(lane_XValue[origLaneNumber-1], transform.position.y, transform.position.z);
-//
-//		myLanePos=origLaneNumber;
-//	}//End
